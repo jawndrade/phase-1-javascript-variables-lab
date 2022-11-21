74babc06869d71702f65297838da9c17620d3d66
Jessica <andradejsscl@gmail.com>
2022-11-20 21:17:19 -0500

    commit tree 27ec18f1bf8ca3a2090e0a45d7178f3247f49636

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution in this file!
+// commenting out code that let us pass the first test let companyName = 'Scuber';
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
